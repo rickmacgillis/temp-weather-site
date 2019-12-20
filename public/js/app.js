5a6f817256c2122b96b180ecb79bdf8ebb7ab4de
Rick Mac Gillis <noreply@rickmacgillis.com>
2019-12-20 17:49:04 -0500

    Misc
    Signed-off-by: Rick Mac Gillis <noreply@rickmacgillis.com>

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@ const searchButton = document.querySelector('#search-button');
 const searchTerm = document.querySelector('#search-term');
 const messageOne = document.querySelector('#message-one');
 const messageTwo = document.querySelector('#message-two');
-
+ 
 searchButton.addEventListener('click', (e) => {
     
     e.preventDefault();
